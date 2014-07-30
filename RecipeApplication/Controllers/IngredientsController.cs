a903b8a1f0f560488c5ba450bfc73f97c6e23bb1
Falcon760 <sfalcon266@hotmail.com>
2014-07-30 11:22:34 -0400

    picture

---
+++ RecipeApplication/Controllers/IngredientsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RecipeApplication;
+
+namespace RecipeApplication.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private RecipeDbEntities db = new RecipeDbEntities();
+
+        // GET: Ingredients
+        public ActionResult Index()
+        {
+            var ingredients = db.Ingredients.Include(i => i.IngredientType).Include(i => i.Recipe);
+            return View(ingredients.ToList());
+        }
+
+        // GET: Ingredients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Create
+        public ActionResult Create()
+        {
+            ViewBag.IngredientTypeId = new SelectList(db.IngredientTypes, "Id", "Name");
+            ViewBag.Id = new SelectList(db.Recipes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Ingredients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,IngredientTypeId")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingredients.Add(ingredient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IngredientTypeId = new SelectList(db.IngredientTypes, "Id", "Name", ingredient.IngredientTypeId);
+            ViewBag.Id = new SelectList(db.Recipes, "Id", "Name", ingredient.Id);
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IngredientTypeId = new SelectList(db.IngredientTypes, "Id", "Name", ingredient.IngredientTypeId);
+            ViewBag.Id = new SelectList(db.Recipes, "Id", "Name", ingredient.Id);
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,IngredientTypeId")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingredient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IngredientTypeId = new SelectList(db.IngredientTypes, "Id", "Name", ingredient.IngredientTypeId);
+            ViewBag.Id = new SelectList(db.Recipes, "Id", "Name", ingredient.Id);
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingredient ingredient = db.Ingredients.Find(id);
+            db.Ingredients.Remove(ingredient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
